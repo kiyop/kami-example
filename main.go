1ff41920af2b7ea3257a50ce25c12e53ac056508
kiyop <tequenica@gmail.com>
2015-02-26 13:09:12 +0900

    change for google app engine, add function to serving static files

--- main.go
+++ main.go
@@ -5,14 +5,30 @@ import (
 	"net/http"
 
 	"github.com/guregu/kami"
+	"github.com/julienschmidt/httprouter"
 	"golang.org/x/net/context"
 )
+
+// ServeFiles is function to serving static multi-files.
+// See `"github.com/julienschmidt/httprouter".ServeFiles()`.
+func ServeFiles(path string, dir http.FileSystem) {
+	kami.Handler().(*httprouter.Router).ServeFiles(path, dir)
+}
+
+// ServeFile is function to serving static single-file.
+func ServeFile(path, file string) {
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, file)
+	})
+}
 
 func hello(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", kami.Param(ctx, "name"))
 }
 
-func main() {
+func init() {
+	http.Handle("/", kami.Handler())
 	kami.Get("/hello/:name", hello)
-	kami.Serve()
+	ServeFiles("/files/*filepath", http.Dir("static-files"))
+	ServeFile("/favicon.ico", "static-files/favicon.ico")
 }
